906105cb6d25dbb75370334aa18ca1cb174fd35e
meryemtnkt <36702284+meryemtnkt@users.noreply.github.com>
2021-05-18 19:43:18 +0300

    Temel java

---
+++ JavaDemos/javademos/src/javademos/loopDemo.java
@@ -0,0 +1,36 @@
+package javademos;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		
+		//for loop
+		for( int i=1; i<=10; i++) {
+			
+			System.out.println(i);
+			
+		}
+		System.out.println("For Döngüsü Bitti");
+		
+		
+		//while loop 
+		int a=0;
+		while(a<10) {
+			System.out.println(a);
+			a+=2;
+		}
+		System.out.println("While Döngüsü Bitti");
+		
+		
+		//do while loop
+		int j=1;
+		do {
+			System.out.println(j);
+			j+=2;
+		}while(j<10) ;
+		System.out.println("Do While Döngüsü Bitti");
+			
+
+	}
+
+}
