75279ef26f8f6efd62b7fb0170b2d95d2babbae7
meryemtnkt <36702284+meryemtnkt@users.noreply.github.com>
2021-05-24 13:57:54 +0300

    sesliharfler
    char ve switch case yapısı kullanımı

--- JavaDemos/javademos/src/javademos/sesliHarfler.java
+++ JavaDemos/javademos/src/javademos/sesliHarfler.java
@@ -3,8 +3,19 @@ package javademos;
 public class sesliHarfler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		char harf='E';
+		
+		switch (harf) {
+		case 'A': 
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalýn sesli harf");
+			break;
+		default:
+				System.out.println("Ýnce sesli harf");
+		}
 	}
 
 }
