465ea0a33832b8841223e398556ee5f2054d387f
meryemtankut <meryemtankut94@gmail.com>
2021-06-16 17:24:54 +0300

    edit

--- day4Homework2/src/day4Homework2/Main.java
+++ day4Homework2/src/day4Homework2/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 
 
-		Customer customer = new Customer(101, "Meryem", "Tankut", LocalDate.of(1994, 2, 2), "20894357042");
+		Customer customer = new Customer(101, "Meryem", "Tankut", LocalDate.of(1994, 2, 2), "01236547890");
 		BaseCustomerManager baseCustomerManager=new StarbucksCustomerManager(new MernisServiceAdapter());
 //				new NeroCustomerManager(new MernisServiceAdapter());
 		
