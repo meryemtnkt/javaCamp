bf53f1f60823818dbd667af58c4cf5b6024563f3
meryemtankut <meryemtankut94@gmail.com>
2021-07-03 14:25:56 +0300

    crossorgin

--- nortwind/src/main/java/kodlamaio/nortwind/api/controllers/ProductsController.java
+++ nortwind/src/main/java/kodlamaio/nortwind/api/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.nortwind.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import kodlamaio.nortwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin
 public class ProductsController {
 	
 	
